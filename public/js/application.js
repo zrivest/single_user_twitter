df07d6e22955d4c898c9f7ff00fd0ce41ee473a7
zrivest <zrivest@gmail.com>
2013-10-22 19:31:44 -0500

    error message now works

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
   $('#tweet_form').on('submit', function(event){
     event.preventDefault();
+
     var data = $("#tweet_form").serialize();
-    console.log(data);
+
     $.post('/tweet', data, function(){
-      $.get("/_all_good", function(response){
+      })
+      .fail("/", function(){
+        alert( "You silly goose, you need to type something!");
+      })
+      .done(function() {
+        $.get("/_all_good", function(response){
         $('#tweet_form').append(response);
       });
     });
